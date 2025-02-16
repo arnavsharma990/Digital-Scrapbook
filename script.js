48057479bb710037b66809a6f45c2a98435ea079
Dhairyagarg812 <dhairyagarg812@gmail.com>
2025-02-16 12:41:54 +0530

    Updated HTML, CSS, and JS for Canva Clone

---
+++ script.js
@@ -0,0 +1,48 @@
+function showTextInput() {
+    console.log("Show Text Input function called"); // Debugging
+    document.getElementById("textInputContainer").style.display = "block";
+}
+function redirect(){
+    let text=document.getElementsByClassName("redi").value;
+    let heading=document.getElementsByClassName("heading");
+    heading.textContent=text;
+}
+function addText() {
+    let inputText = document.getElementById("textInput").value;
+    let text = document.createElement("p");
+    text.classList.add("element");
+    text.textContent = inputText;
+    document.getElementById("canvas").appendChild(text);
+    document.getElementById("textInput").value = "";
+    document.getElementById("textInputContainer").style.display = "none";
+}
+
+
+
+function addImage(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            let img = document.createElement("img");
+            img.src = e.target.result;
+
+            img.style.maxWidth = "50vw";
+            img.style.maxHeight = "50vh";
+            img.style.margin = "5px";
+            document.getElementById("canvas").appendChild(img);
+        };
+
+        reader.readAsDataURL(file);
+    }
+}
+
+function addEmoji() {
+    let emojiSelector = document.getElementById("emojiSelector");
+    let selectedEmoji = emojiSelector.value;
+    let emoji = document.createElement("p");
+    emoji.style.textAlign="center"
+    emoji.classList.add("element");
+    emoji.textContent = selectedEmoji;
+    document.getElementById("canvas").appendChild(emoji);
+}
